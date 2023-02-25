00f18a28abe75c3d6d0ae99379579fa8a0eed676
carinaioana <carinasrb@yahoo.com>
2023-02-25 15:48:20 +0200

    lab1

--- src/Lab1/compulsory/Main.java
+++ src/Lab1/compulsory/Main.java
@@ -26,5 +26,6 @@ public class Main {
             sum = suma1;
         }
         System.out.println( "Willy-nilly, this semester I will learn " + languages[sum]);
+        //everytime will print java
     }
 }
