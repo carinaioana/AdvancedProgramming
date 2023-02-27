dc8027b0243c70f0ddc20649632360ba13fa9757
carinaioana <carinasrb@yahoo.com>
2023-02-27 21:11:40 +0200

    tema1

--- src/Lab1/homework/Main.java
+++ src/Lab1/homework/Main.java
@@ -1,19 +1,46 @@
 package Lab1.homework;
 
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void matrix(int n){
-        int m[][] = new int [n][n];
-        for(int i = 0;i < n;i++) {
+    public static void display(int m[][],int n) {
+
+        for (int i = 0; i < n; i++) {
+
             for (int j = 0; j < n; j++) {
-                m[i][j] = (i + j) % n;
-                System.out.print(m[i][j]);
+
+                String number = Integer.toString(m[i][j]);
+                System.out.print(number + " ");
             }
             System.out.println();
         }
     }
+    public static int[][] matrix(int n){
+
+        int m[][] = new int [n][n];
+        for(int i = 0;i < n;i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                m[i][j] = (i + j) % n;
+            }
+        }
+        return m;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        final long b = System.nanoTime();
+
         String s = args[0];
         int n = Integer.parseInt(s);
+        if (n >= 30000) {
+            final long time= System.nanoTime() - b;
+            System.out.println(time);
+            return;
+        }
+        int m[][] = matrix(n);
+        display(m,n);
+
+
     }
 }
