cd8d2b0d749cf1b976daa94d1ba29cad8a4c3129
carinaioana <carinasrb@yahoo.com>
2023-04-28 22:53:39 +0300

    lab8h

---
+++ src/main/java/Lab8/homework/interfaces/AlbumsDAO.java
@@ -0,0 +1,7 @@
+package Lab8.homework.interfaces;
+
+import Lab8.homework.classes.Albums;
+
+public interface AlbumsDAO extends DAO<Albums> {
+
+}
---
+++ src/main/java/Lab8/homework/interfaces/ArtistsDAO.java
@@ -0,0 +1,6 @@
+package Lab8.homework.interfaces;
+
+import Lab8.homework.classes.Artists;
+
+public interface ArtistsDAO extends DAO<Artists>{
+}
