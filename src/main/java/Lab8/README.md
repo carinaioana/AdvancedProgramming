33f1010c235c0dc1938269b518560c55f5ead317
carinaioana <carinasrb@yahoo.com>
2023-04-28 22:50:40 +0300

    lab8h

--- src/main/java/Lab8/README.md
+++ src/main/java/Lab8/README.md
@@ -1,4 +1,5 @@
 JDBC
+
 Write an application that allows to connect to a relational database by using JDBC, submit SQL statements and display the results.
 
 The main specifications of the application are:
@@ -17,3 +18,12 @@ an associative (junction) table in order to store each album genres
 -[x] Create a singleton class in order to manage a connection to the database.
 -[x] Create DAO classes that offer methods for managing artists, genres and albums (at least one).
 -[x] Implement a simple test using your classes.
+
+## Optional
+
+-----
+
+- [x] Create an object-oriented model of the data managed by the Java application.
+- [x] Implement all the DAO classes.
+-[x] Use a connection pool in order to manage database connections, such as C3PO, HikariCP or Apache Commons DBCP.
+- [x] Create a tool to import data from a real dataset, such as Rolling Stone's 500 Greatest Albums of All Time (or other)
---
+++ src/main/java/Lab8/homework/classes/AlbumsDAOImpl.java
@@ -0,0 +1,126 @@
+package Lab8.homework.classes;
+
+import Lab8.homework.interfaces.AlbumsDAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumsDAOImpl implements AlbumsDAO {
+
+    //CRUD - Retrieve
+    @Override
+    public Albums get(int id) throws SQLException {
+        Connection connection = Database.getConnection();
+        Albums albums = null;
+        String sql = "SELECT id, release_year, title, artist, genre FROM albums WHERE id = ? ;";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()) {
+            Integer oid = resultSet.getInt("id");
+            Integer ReleaseYear = resultSet.getInt("release_year");
+            String Title = resultSet.getString("title");
+            String Artist = resultSet.getString("artist");
+            String Genre = resultSet.getString("genre");
+
+            albums = new Albums(oid, ReleaseYear, Title, Artist, Genre);
+        }
+        Database.closeResultSet(resultSet);
+        Database.closePreparedStatement(preparedStatement);
+        Database.closeConnection(connection);
+
+        return  albums;
+    }
+    //CRUD - Retrieve All
+    @Override
+    public List<Albums> getAll() throws SQLException {
+        Connection connection = Database.getConnection();
+
+        String sql = "SELECT id, release_year, title, artist, genre FROM albums;";
+
+        List<Albums> albumList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()){
+
+            Integer oid = resultSet.getInt("id");
+            Integer ReleaseYear = resultSet.getInt("release_year");
+            String Title = resultSet.getString("title");
+            String Artist = resultSet.getString("artist");
+            String Genre = resultSet.getString("genre");
+
+            Albums album = new Albums(oid, ReleaseYear, Title, Artist, Genre);
+            albumList.add(album);
+        }
+        return albumList;
+    }
+    //CRUD - Create or Update
+    @Override
+    public int save(Albums albums) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int insert(Albums albums) throws SQLException {
+        Connection con = Database.getConnection();
+
+        String sql = "INSERT INTO albums (id, release_year, title, artist, genre) VALUES (?, ?, ?, ?, ?)";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setInt(1, albums.getId());
+        ps.setInt(2, albums.getRelease_year());
+        ps.setString(3, albums.getTitle());
+        ps.setString(4, albums.getArtist());
+        ps.setString(5, albums.getGenre());
+
+        int result = ps.executeUpdate();
+
+        Database.closePreparedStatement(ps);
+        Database.closeConnection(con);
+
+        return result;
+    }
+
+    @Override
+    public int update(Albums albums) throws SQLException {
+        Connection connection = Database.getConnection();
+
+        String sql = "UPDATE albums set release_year = ?, title = ?, artist = ?, genre = ? where id = ?";
+
+        PreparedStatement ps = connection.prepareStatement(sql);
+
+        ps.setInt(1, albums.getRelease_year());
+        ps.setString(2, albums.getTitle());
+        ps.setString(3, albums.getArtist());
+        ps.setString(4, albums.getGenre());
+        ps.setInt(5, albums.getId());
+
+        int result = ps.executeUpdate();
+
+        Database.closePreparedStatement(ps);
+        Database.closeConnection(connection);
+
+        return result;
+    }
+    //CRUD - Delete
+    @Override
+    public int delete(Albums albums) throws SQLException {
+        Connection connection = Database.getConnection();
+
+        String sql = "DELETE FROM albums WHERE id = ?";
+
+        PreparedStatement ps = connection.prepareStatement(sql);
+
+        ps.setInt(1, albums.getId());
+
+        int result = ps.executeUpdate();
+
+        Database.closePreparedStatement(ps);
+        Database.closeConnection(connection);
+
+        return result;
+    }
+}
