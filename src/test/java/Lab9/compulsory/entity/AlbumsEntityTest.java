719512947470eff41475bec2c263380015caa80d
carina.ioana <carinasrb@icloud.com>
2023-05-03 00:54:45 +0300

    lab9

---
+++ src/test/java/Lab9/compulsory/entity/AlbumsEntityTest.java
@@ -0,0 +1,58 @@
+package Lab9.compulsory.entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumsEntityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getReleaseYear() {
+    }
+
+    @Test
+    void setReleaseYear() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getArtist() {
+    }
+
+    @Test
+    void setArtist() {
+    }
+
+    @Test
+    void getGenres() {
+    }
+
+    @Test
+    void setGenres() {
+    }
+}
